3563ab8d2508d4094feeca332a0f4d20cefbca4f
jason <394313@bsd48.org>
2020-10-08 09:37:18 -0700

    started

--- classes.cpp
+++ classes.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include <vector>
 #include <parent.h>
 #include <videogame.h>
---
+++ parent.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include <parent.h>
+
+using namespace std;
+
+class parent{
+public:
+  parent(){
+    
+  }
+  char parent::getTitle(){
+    return title;
+  }
+  int Media::getYear(){
+    return year;
+  }
+  
+}
